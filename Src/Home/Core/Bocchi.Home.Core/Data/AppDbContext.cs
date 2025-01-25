9beb937b5d93711076c731cb5a60f47d5d6a466f
大魔王有木桑 <i@yomu.moe>
2025-01-25 02:25:49 +0000

    添加对 Nekonya.Common 的引用，公开 AppDbContext 类，并增强数据库连接池支持

--- Src/Home/Core/Bocchi.Home.Core/Data/AppDbContext.cs
+++ Src/Home/Core/Bocchi.Home.Core/Data/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace Bocchi.Home.Core.Data;
 /// <summary>
 /// Bocchi Home 主数据库上下文
 /// </summary>
-internal class AppDbContext : DbContext
+public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
--- Src/Home/Infrastructure/Bocchi.Home.Infrastructure/InfrastructureModule.cs
+++ Src/Home/Infrastructure/Bocchi.Home.Infrastructure/InfrastructureModule.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nekonya;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Bocchi.Home.Core.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bocchi.Home.Infrastructure;
 
@@ -17,5 +21,67 @@ public static class InfrastructureModule
     public static void AddBocchiAppDatabase(in IServiceCollection services, in IConfiguration configuration)
     {
         bool usePool = configuration.GetValue<bool>("Database:Bocchi:UseConnectionPool");
+        int poolSize = configuration.GetValue<int>("Database:Bocchi:PoolSize", 0);
+        var (dbProvider, connectionString) = GetConnectionString(configuration, out string confKey);
+        if (dbProvider is DatabaseProvider.Unknown || connectionString is null)
+        {
+            throw new InvalidOperationException($"Invalid database provider or connection string, configuration key: {confKey}");
+        }
+
+        if (usePool && poolSize > 0)
+        {
+            services.AddDbContextPool<AppDbContext>(options =>{
+                switch (dbProvider)
+                {
+                    case DatabaseProvider.Sqlite:
+                        options.UseSqlite(connectionString);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unsupported database provider: {dbProvider}");
+                }
+            }, poolSize);
+        }
+        else
+        {
+            services.AddDbContext<AppDbContext>(options =>{
+                switch (dbProvider)
+                {
+                    case DatabaseProvider.Sqlite:
+                        options.UseSqlite(connectionString);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unsupported database provider: {dbProvider}");
+                }
+            });
+        }        
     }
+
+
+    private static (DatabaseProvider, string?) GetConnectionString(IConfiguration configuration, out string confKey)
+    {
+        var providerTypeStr = configuration.GetValue<string>("Database:Bocchi:Type");
+        DatabaseProvider dbProvider = providerTypeStr?.ToLower() switch
+        {
+            "sqlite" or "sqlite3" => DatabaseProvider.Sqlite,
+            "sqlserver" or "mssql" => DatabaseProvider.SqlServer,
+            "postgresql" => DatabaseProvider.PostgreSql,
+            "mysql" or "mariadb" => DatabaseProvider.MySql,
+            _ => DatabaseProvider.Unknown
+        };
+        
+        confKey = dbProvider is not DatabaseProvider.Unknown ? $"Database:Bocchi:ConnectionString:{dbProvider}" : string.Empty;
+        return (dbProvider, configuration.GetValue<string>(confKey));
+    }
+
+
+    private enum DatabaseProvider
+    {
+        Unknown,
+     
+        Sqlite,
+        SqlServer,
+        PostgreSql,
+        MySql
+    }
+    
 }
